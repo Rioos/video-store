550abf8f32170da05e1a13a142b05a34d0a693b2
Ricardo Maisonnave <4395602+Rioos@users.noreply.github.com>
2024-07-14 01:27:57 -0300

    chore: adds option for front-end URL env var

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend's origin
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 
